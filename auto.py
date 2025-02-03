cfafff464f7749c976877768956d440e39fe908e
ElliotBrouillard <2162349@cegepoutaouais.qc.ca>
2025-02-03 16:17:15 -0500

    création de la classe auto

---
+++ auto.py
@@ -0,0 +1,14 @@
+class Auto:
+    """
+    Classe auto qui permet de choisir une auto
+    """
+    def __init__(self, marque, modele, prix, nombre_portes, type_auto):
+        self.marque = marque
+        self.modele = modele
+        self.prix = prix
+        self.nombre_portes = nombre_portes
+        self.type_auto = type_auto
+
+    def se_deplacer(self):
+        print("L'auto se déplace")
+
